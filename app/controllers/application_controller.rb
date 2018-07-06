d59219f5bc1e21b995e6bb60d9f85e09fd879e18
Héctor Espinoza <hectoraee@gmail.com>
2018-07-06 17:28:52 -0400

    add api handler helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  require 'rest-client'
+
+  def call_api(url)
+    response = RestClient::Request.execute(
+        method: :get,
+        url: url,
+        headers: {'Content-Type': 'application/json'}
+    )
+    JSON.parse(response)
+  end
 end
