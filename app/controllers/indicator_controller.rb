5b44e09172041cec6c4faf13598b616c135c80fa
Héctor Espinoza <hectoraee@gmail.com>
2018-07-06 23:32:33 -0400

    add view

--- app/controllers/indicator_controller.rb
+++ app/controllers/indicator_controller.rb
@@ -1,5 +1,9 @@
 class IndicatorController < ApplicationController
   def index
+
+  end
+
+  def get_uf
     @data = call_api('https://api.sbif.cl/api-sbifv3/recursos_api/uf/periodo/2010/01/2010/03')
   end
 end
