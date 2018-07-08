b622f4b393ef9e1f0dfe25dd448ad4b61d14e155
Héctor Espinoza <hectoraee@gmail.com>
2018-07-07 22:50:23 -0400

    add table for uf requested data

--- app/controllers/indicator_controller.rb
+++ app/controllers/indicator_controller.rb
@@ -1,9 +1,20 @@
 class IndicatorController < ApplicationController
+  require 'date'
+
   def index
 
   end
 
   def get_uf
-    @data = call_api('https://api.sbif.cl/api-sbifv3/recursos_api/uf/periodo/2010/01/2010/03')
+    from = (params[:uf][:from].to_s.split('-'))
+    to = (params[:uf][:to].to_s.split('-'))
+    from_date = Date.parse(params[:uf][:from])
+    to_date = Date.parse(params[:uf][:to])
+
+    data = call_api("https://api.sbif.cl/api-sbifv3/recursos_api/uf/periodo/#{from[0]}/#{from[1]}/#{to[0]}/#{to[1]}")['UFs']
+    array_data = data.map{|x| [x['Fecha'],x['Valor']]}
+    @final_data = array_data.map {|date,value| [date,value] if Date.parse(date) >= from_date && Date.parse(date) <= to_date}.compact
+
+    render :template => "indicator/index"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :indicator
+  post 'indicator/get_uf'
   #get 'indicator/index'
 end
