25bbcc916e7bcf6e37fbb69c381c199a8db488e3
Héctor Espinoza <hectoraee@gmail.com>
2018-07-07 23:57:35 -0400

    add prom

--- app/controllers/indicator_controller.rb
+++ app/controllers/indicator_controller.rb
@@ -14,6 +14,12 @@ class IndicatorController < ApplicationController
     data = call_api("https://api.sbif.cl/api-sbifv3/recursos_api/uf/periodo/#{from[0]}/#{from[1]}/#{to[0]}/#{to[1]}")['UFs']
     array_data = data.map{|x| [x['Fecha'],x['Valor']]}
     @final_data = array_data.map {|date,value| [date,value] if Date.parse(date) >= from_date && Date.parse(date) <= to_date}.compact
+    sum = 0.0
+
+    @final_data.each do |date,value|
+      sum += value.delete('.').split(',').join('.').to_f
+    end
+    @prom = (sum / @final_data.count).round(2)
 
     render :template => "indicator/index"
   end
