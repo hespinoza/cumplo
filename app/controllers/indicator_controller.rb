52e90fa8219f52e4ace740273a9069365f2a25a4
Héctor Espinoza <hectoraee@gmail.com>
2018-07-08 02:01:11 -0400

    add logo, menu and charts

--- app/controllers/indicator_controller.rb
+++ app/controllers/indicator_controller.rb
@@ -14,12 +14,12 @@ class IndicatorController < ApplicationController
     data = call_api("https://api.sbif.cl/api-sbifv3/recursos_api/uf/periodo/#{from[0]}/#{from[1]}/#{to[0]}/#{to[1]}")['UFs']
 
     array_data = data.map{|x| [x['Fecha'],x['Valor']]}
-    @final_data = array_data.map {|date,value| [date,value] if Date.parse(date) >= from_date && Date.parse(date) <= to_date}.compact
+    @final_data = array_data.map {|date,value| [date,(value.delete('.').split(',').join('.').to_f)] if Date.parse(date) >= from_date && Date.parse(date) <= to_date}.compact
     sum = 0.0
     selected_values = []
 
     @final_data.each do |date,value|
-      num_value = value.delete('.').split(',').join('.').to_f
+      num_value = value
       sum += num_value
       selected_values << num_value
     end
@@ -29,6 +29,7 @@ class IndicatorController < ApplicationController
     @min_value = selected_values.min
     @from_value = params[:uf][:from] == '' ? Date.today.to_s : params[:uf][:from]
     @to_value = params[:uf][:to] == '' ? Date.today.to_s : params[:uf][:to]
+
     render :template => "indicator/index"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'indicator#index'
+
   resources :indicator
   post 'indicator/get_uf'
   #get 'indicator/index'
